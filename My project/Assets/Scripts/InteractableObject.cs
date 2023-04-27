48c6dc2ddad52aaf94433c9b2f7287c3c8b293dd
Dolgormaa <d0g11.7.100@gmail.com>
2023-04-27 17:42:46 +0900

    update key usage

--- My project/Assets/Scripts/InteractableObject.cs
+++ My project/Assets/Scripts/InteractableObject.cs
@@ -5,24 +5,25 @@ using UnityEngine;
 
 public class InteractableObject : CollidableObject
 {
-    // private bool z_Interacted = false;
+    private bool z_Interacted = false;
     public TMP_Text pressKey;
     protected override void OnCollided(GameObject collidedObject)
     {
-        pressKey.text = "Press 'E'";
+        if (!z_Interacted)
+        {
+            pressKey.text = "Press 'E'";
+        }
+        
         if (Input.GetKey(KeyCode.E))
         {
+            z_Interacted = true;
             OnInteract();
+            pressKey.text = " ";
         }
     }
 
     private void OnInteract()
     {
-        // if (!z_Interacted)
-        // {
-        //     z_Interacted = true;
-        // }
-        pressKey.text = " ";
         Debug.Log("Interact with" + name);
     }
 }
