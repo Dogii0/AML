49b6a06ba69a1942084981ebcf1d642a871b76ef
Mike <michaelaugusta15@gmail.com>
2023-05-03 13:16:01 +0900

    michael

---
+++ My project/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    public int patrolDestination;
+    public Transform playerTransform;
+    public bool isChasing;
+    public float ChaseDistance;
+    private float distance;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isChasing)
+        {
+            distance = Vector2.Distance(transform.position, playerTransform.position);
+            Vector2 direction = playerTransform.position - transform.position;
+            direction.Normalize();
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            if (distance < ChaseDistance)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, playerTransform.position,
+                    moveSpeed * Time.deltaTime);
+                if(playerTransform.position.x < transform.position.x)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                if(playerTransform.position.x > transform.position.x)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                
+
+            }
+            else
+            {
+                isChasing = false;
+                if (patrolDestination == 0)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+
+            }
+            
+        }
+        else
+        {
+            if (Vector2.Distance(transform.position, playerTransform.position) < ChaseDistance)
+            {
+                isChasing = true;
+            }
+            if (patrolDestination == 0)
+            {
+                transform.position =
+                    Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                    patrolDestination = 1;
+                }
+            }
+        
+            if (patrolDestination == 1)
+            {
+                transform.position =
+                    Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                    patrolDestination = 0;
+                }
+            }
+        }
+        
+    }
+
+
+}
