51926e103c9495d81d22360a63f8a3af73a1e2fa
Jasper <jasperyap2001@gmail.com>
2023-05-07 20:55:10 +0900

    Movement.cs changes
    Added code for movement detection for animation

--- My project/Assets/Scripts/Movement.cs
+++ My project/Assets/Scripts/Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Animator animator;
     public float speed;
     void Start()
     {
@@ -13,5 +14,16 @@ public class Movement : MonoBehaviour
     {
         transform.Translate(Vector3.right*Time.deltaTime*speed*Input.GetAxis("Horizontal"));
         transform.Translate(Vector3.up*Time.deltaTime*speed*Input.GetAxis("Vertical"));
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        
+        animator.SetFloat("moveX", moveX);
+        animator.SetFloat("moveY", moveY);
+        
+        bool isMoving = !Mathf.Approximately(moveX, 0f);
+        if (isMoving == false) isMoving = !Mathf.Approximately(moveY, 0f);
+
+        animator.SetBool("isMoving", isMoving);
     }
 }
