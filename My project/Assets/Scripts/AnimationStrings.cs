0819c51e5ec48a51afa1fb77c47774223165eefb
Jasper <jasperyap2001@gmail.com>
2023-06-06 23:30:44 +0900

    Revert "Merge branch 'Dogii' into Jasper"
    This reverts commit da681b8952fa2de1f6effd7172a684939e14fadf, reversing
    changes made to a97cbdbffabe907da860094bd9e87b9b9e490173.

--- My project/Assets/Scripts/AnimationStrings.cs
+++ My project/Assets/Scripts/AnimationStrings.cs
@@ -7,4 +7,6 @@ internal class AnimationStrings
     internal static string moveY = "moveY";
     internal static string attackTrigger = "attackTrigger";
     internal static string playerDirection = "playerDirection";
+    internal static string weaponType = "weaponType";
+    internal static string flipAnimation = "flipAnimation";
 }
