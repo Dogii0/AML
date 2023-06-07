18d9c37046081a132ce9b511c4f316ef3308f4df
Mike <michaelaugusta15@gmail.com>
2023-06-07 14:43:16 +0900

    hihi

--- My project/Assets/Scripts/Boss/Boss_Script.cs
+++ My project/Assets/Scripts/Boss/Boss_Script.cs
@@ -10,10 +10,17 @@ public class Boss_Script : MonoBehaviour
     public Transform bulletpos;
     private float timer;
     public GameObject player;
-    
+    public HealthBarBoss Healthbarboss;
+    public float Hitpoints;
+    public float max_HP;
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Start()
+    {
+        Healthbarboss.SetHealth(Hitpoints, max_HP);
     }
 
     void Update()
@@ -34,6 +41,16 @@ public class Boss_Script : MonoBehaviour
     void shoot()
     {
         Instantiate(bullet, bulletpos.position, Quaternion.identity);
+    }
+
+    public void hit(float damage)
+    {
+        Hitpoints -= damage;
+
+        if (Hitpoints == 0)
+        {
+            Destroy(gameObject);
+        }
     }
     
 }
